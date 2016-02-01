56dab93672ccee41c749b383fea391690e92e9eb
Yondy Kang <yokangy@gmail.com>
2016-01-31 22:43:06 -0800

    Created WeatherList component which currently displays a list of cities.

---
+++ src/containers/weather_list.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class WeatherList extends Component {
+  renderWeather(cityData){
+    const cityName = cityData.city.name;
+
+    return (
+      <tr key={cityName}>
+        <td>{cityName}</td>
+      </tr>
+    );
+  }
+
+
+  render() {
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.weather.map(this.renderWeather)}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+function mapStateToProps({ weather }) {
+  return { weather }; // { weather } === {weather:weather}
+}
+
+export default connect(mapStateToProps)(WeatherList);
