8f846e65d8aded88dd0ef36f5f13564f6211578e
Yondy Kang <yokangy@gmail.com>
2016-02-06 23:44:24 -0800

    Added google maps to the weather app.

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
   renderWeather(cityData){
@@ -8,10 +9,12 @@ class WeatherList extends Component {
     const temps = cityData.list.map(weather => weather.main.temp);
     const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
+    const { lon, lat } = cityData.city.coord;
+    //const lat = cityData.city.coord.lat;
 
     return (
       <tr key={cityName}>
-        <td>{cityName}</td>
+        <td><GoogleMap lon={lon} lat={lat} /></td>
         <td><Chart data={temps} color="blue" units="K" /></td>
         <td><Chart data={pressures} color="black" units="hPa" /></td>
         <td><Chart data={humidities} color="red" units="%" /></td>
